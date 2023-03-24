eb9123c504cf1c02c4e5c93dd4430be6c07dd4e8
Somporn Tongsub <96121622+The-Falcon-Inspiron-Team@users.noreply.github.com>
2023-03-24 23:51:39 +0700

    Update speaking  python.py

--- speaking  python.py
+++ speaking  python.py
@@ -1,14 +1,14 @@
 from gtts import gTTS
 import os
 
-# The text to convert to speech (in Thai)
+
 text = "สวัสดีค่ะ"
 
-# Create a gTTS object
+
 tts = gTTS(text=text, lang='th')
 
-# Save the speech to a file
+
 tts.save("Sound_record.mp3")
 
-# Play the speech
+
 os.system("Sound_record.mp3")
